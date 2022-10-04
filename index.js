2c98af763e029afa1a580eff3f58fcc9f42e8a2a
elizabethlosotua <elizabethlosotua@gmail.com>
2022-10-04 14:26:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// code your solution here
+function findMatching (drivernames, firstname){
+    const names = drivernames.filter(function(item){
+        return item.toLowerCase() === firstname.toLowerCase()
+
+
+    })
+    return names
+}
+function fuzzyMatch(drivers, lookFor){
+    return drivers.filter(nameOfItem => nameOfItem.startsWith(lookFor) )
+}
+
+function matchName(drivernames, name){
+    const filteredArray = drivernames.filter(function(list){
+        return list.name === name;
+    })
+    return filteredArray
+}
+phase-1-array-filter-method-lab  
